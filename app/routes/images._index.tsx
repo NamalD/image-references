955541d5414506a23890f748d43c279de328d95a
Namal <hi@namal.dev>
2023-09-09 14:36:54 +0100

    Styling improvement^s

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -16,20 +16,30 @@ export default function Images() {
   // TODO: Delete tags
   // TODO: Delete images
   // TODO: Edit image metadata
+  // TODO: Refactor each image to page
 
   return (
-    <main>
-      <h1>Images</h1>
+    <main className="m-3">
+      <h1 className="text-xl mb-3">Images</h1>
+      <div className="grid grid-cols-3 gap-3">
       {
         images.map(image => (
           <div key={image.id}>
-            <h2>{image.name}</h2>
-            <img src={image.path} alt={image.name} />
+            <h2 className="text-lg text-gray-900">
+              {image.name}
+            </h2>
+            <img
+              src={image.path}
+              alt={image.name}
+              className="h-1/3"
+            />
             {
-              <ul>
+              <ul className="text-gray-600 flex flex-wrap items-center">
                 {
                   image.tags.map(tag => (
-                    <li key={tag.id}>{tag.name}</li>
+                    <li key={tag.id}>
+                      <span className="italic mr-4">{tag.name}</span>
+                    </li>
                   ))
                 }
               </ul>
@@ -37,6 +47,7 @@ export default function Images() {
           </div>
         ))
       }
+      </div>
     </main>
   );
 }
