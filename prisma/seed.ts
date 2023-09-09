723a835c84ac24d0a87e221f37fac396731e83bc
Namal <hi@namal.dev>
2023-09-09 14:07:51 +0100

    Fix file paths

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,7 +6,7 @@ const images = [
   {
     id: 1,
     name: 'Banks of Acheron',
-    path: 'public/img/banks_of_acheron.jpg',
+    path: 'img/banks_of_acheron.png',
     tags: [
       { name: 'Painting' },
       { name: 'Greek Myth' },
@@ -16,7 +16,7 @@ const images = [
   {
     id: 2,
     name: 'Animals in Moonlit Forest',
-    path: 'public/img/animals_in_moonlit_forest.jpg',
+    path: 'img/animals_in_moonlit_forest.png',
     tags: [
       { name: 'Animals' },
       { name: 'Forest' },
