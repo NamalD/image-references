f78b585cf9f5e5ea7c5331f16f1830ee38ac6743
Namal <hi@namal.dev>
2023-09-10 15:57:59 +0100

    Button styling

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -2,8 +2,11 @@ import { Link } from "@remix-run/react";
 
 export default function ImagesIndex() {
   return (
-    <div>
-      <Link to="upload" className="text-blue-600">
+    <div className="mb-3">
+      <Link
+        to="upload"
+        className="bg-orange-600 text-white px-4 py-2 rounded hover:bg-orange-700 mt-3"
+      >
         Upload
       </Link>
     </div>
