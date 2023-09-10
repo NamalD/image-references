5226cdb32dce989a9c798da5950aaa811d728fd5
Namal <hi@namal.dev>
2023-09-10 15:13:32 +0100

    Support error messages

--- app/components/FormElement.tsx
+++ app/components/FormElement.tsx
@@ -3,13 +3,18 @@ import React from "react";
 interface FormElementProps {
   label: string;
   children: React.ReactNode;
+  error?: string | null;
 }
 
-export function FormElement({ label, children }: FormElementProps) {
+export function FormElement({ label, error, children }: FormElementProps) {
   return (
     <p>
       <label>
-        {label}
+        {label}: {" "}
+        {
+          error &&
+          <em className="text-red-600">{error}</em>
+        }
         {children}
       </label>
     </p>
