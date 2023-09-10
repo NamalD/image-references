56ca3d82b3c37dcd2f37d2fa6828f8f6527ecbce
Namal <hi@namal.dev>
2023-09-10 17:27:23 +0100

    Link to image by ID

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -1,5 +1,5 @@
 import { json, LoaderArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData } from "@remix-run/react";
 import { getImages } from "~/routes/images.server";
 
 export const loader = async ({ params }: LoaderArgs) => {
@@ -10,17 +10,37 @@ export default function ImagesIndex() {
   const { images } = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex flex-wrap w-11/12 justify-center gap-3">
+    <div>
       {
-        images.map(image => (
-          <img
-            key={image.id}
-            src={image.path}
-            alt={image.name}
-            className="h-60 w-60 object-cover"
-          />
-        ))
+        images.length > 0 && (
+          <Form method="delete">
+            <button
+              type="submit"
+              className="bg-red-600 text-white px-4 py-2 rounded hover:bg-orange-700 mt-3"
+            >
+              Delete All
+            </button>
+          </Form>
+        )
       }
+
+      <div className="flex flex-wrap w-11/12 justify-center gap-3">
+
+        {
+          images.map(image => (
+            <Link
+              to={image.id.toString()}
+              key={image.id}
+            >
+              <img
+                src={image.path}
+                alt={image.name}
+                className="h-60 w-60 object-cover"
+              />
+            </Link>
+          ))
+        }
+      </div>
     </div>
   );
 }
