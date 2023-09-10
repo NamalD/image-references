488e89026d30830811738ee122aa433ad43cd537
Namal <hi@namal.dev>
2023-09-10 17:14:10 +0100

    Refactor index page

--- app/routes/images._index.tsx
+++ app/routes/images._index.tsx
@@ -1,54 +1,7 @@
-import { ActionArgs, json } from "@remix-run/node";
-import { Form, Link, useLoaderData } from "@remix-run/react";
-import { prisma } from "~/db.server";
-
-export const loader = async () => {
-  const images = await prisma.image.count();
-  return json({ images });
-}
-
-export const action = async ({ request }: ActionArgs) => {
-  if (request.method !== "DELETE") {
-    return json({ message: "Method not allowed" }, { status: 405 });
-  }
-
-  const images = await prisma.image.findMany({});
-  for (const image of images) {
-    console.log(`Deleting image ${image.id}`);
-    await prisma.image.delete({
-      where: {
-        id: image.id
-      }
-    });
-  }
-
-  return null;
-}
-
 export default function ImagesIndex() {
-  const { images } = useLoaderData<typeof loader>();
-
+  // TODO: Move all images to this page
   return (
-    <div className="flex gap-1 mb-3">
-      <Link
-        to="upload"
-        className="bg-green-600 text-white px-4 py-2 rounded hover:bg-orange-700 mt-3"
-      >
-        Upload
-      </Link>
-
-      {
-        images > 0 && (
-          <Form method="delete">
-            <button
-              type="submit"
-              className="bg-red-600 text-white px-4 py-2 rounded hover:bg-orange-700 mt-3"
-            >
-              Delete All
-            </button>
-          </Form>
-        )
-      }
+    <div>
     </div>
   )
 }
--- app/routes/images.tsx
+++ app/routes/images.tsx
@@ -1,14 +1,34 @@
-import { json } from "@remix-run/node";
-import { Outlet, useLoaderData } from "@remix-run/react";
+import { ActionArgs, json, LoaderArgs } from "@remix-run/node";
+import { Form, Link, Outlet, useLoaderData } from "@remix-run/react";
+import { prisma } from "~/db.server";
 import { getImages } from "~/routes/images.server";
 
-export const loader = async () => {
-  return json({ images: await getImages() });
+export const loader = async ({ params }: LoaderArgs) => {
+  return json({ id: params.id, images: await getImages() });
 };
+
+export const action = async ({ request }: ActionArgs) => {
+  if (request.method !== "DELETE") {
+    return json({ message: "Method not allowed" }, { status: 405 });
+  }
+
+  const images = await prisma.image.findMany({});
+  for (const image of images) {
+    console.log(`Deleting image ${image.id}`);
+    await prisma.image.delete({
+      where: {
+        id: image.id
+      }
+    });
+  }
+
+  return null;
+}
 
 export default function Images() {
   const { images } = useLoaderData<typeof loader>();
 
+  // TODO: Single image page
   // TODO: Edit image metadata
   // TODO: Refactor each image to page
   // TODO: Lightbox
@@ -18,6 +38,29 @@ export default function Images() {
   return (
     <main className="m-3">
       <h1 className="text-xl mb-3">Images</h1>
+
+      <div className="flex gap-1 mb-3">
+        <Link
+          to="upload"
+          className="bg-green-600 text-white px-4 py-2 rounded hover:bg-orange-700 mt-3"
+        >
+          Upload
+        </Link>
+
+        {
+          images.length > 0 && (
+            <Form method="delete">
+              <button
+                type="submit"
+                className="bg-red-600 text-white px-4 py-2 rounded hover:bg-orange-700 mt-3"
+              >
+                Delete All
+              </button>
+            </Form>
+          )
+        }
+      </div>
+
       <Outlet />
       <div className="flex flex-wrap w-11/12 justify-center gap-3">
         {
