830537bfff59e91a1d781a3fd431f458051053bb
Namal <hi@namal.dev>
2023-09-10 17:47:42 +0100

    Support classnames for inputs

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -4,12 +4,13 @@ interface InputProps {
   name: string;
   type?: InputHTMLAttributes<HTMLInputElement>['type'];
   accept?: string;
+  className?: string;
 }
 
-export const Input = ({ name, type = 'text', accept }: InputProps) =>
+export const Input = ({ name, type = 'text', accept, className }: InputProps) =>
   <input
     type={type}
     name={name}
     accept={accept}
-    className="border border-gray-300 w-full px-2 py-1"
+    className={`border border-gray-300 w-full px-2 py-1 ${className}`}
   />;
