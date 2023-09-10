6c7e2209e5c1b8c301c089b7becd3f6c1f863065
Namal <hi@namal.dev>
2023-09-10 11:19:18 +0100

    Support type and accept props

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,6 +1,15 @@
-export const Input = (props: { name: string }) =>
+import { InputHTMLAttributes } from "react";
+
+interface InputProps {
+  name: string;
+  type?: InputHTMLAttributes<HTMLInputElement>['type'];
+  accept?: string;
+}
+
+export const Input = ({ name, type = 'text', accept }: InputProps) =>
   <input
-    type="text"
-    name={props.name}
+    type={type}
+    name={name}
+    accept={accept}
     className="border border-gray-300 w-full px-2 py-1"
   />;
